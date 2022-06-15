862998c961679380e643127ae1ef8a5a10ffa7d8
Makso2316 <70938221+Makso2316@users.noreply.github.com>
2022-06-15 13:42:35 +0200

    Sekcja 2 Lekcja 12

--- TGK/Assets/Scripts/Drops.cs
+++ TGK/Assets/Scripts/Drops.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Drops : MonoBehaviour
 {
+     MeshRenderer renderer;
+     Rigidbody rigidbody;
    [SerializeField] float timeToWait = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
@@ -16,7 +22,8 @@ public class Drops : MonoBehaviour
     {
         if (Time.time > timeToWait)
         {
-            Debug.Log("3 secends has elapsde");
+        renderer.enabled = true;
+        rigidbody.useGravity = true;
         }
     }
     
